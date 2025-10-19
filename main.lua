1fadbac426877883ddcc4a6f8bb300db09dc43ac
cheathubtech <cidkagenoumrhurumia@gmail.com>
2025-10-19 03:07:03 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+-- Загружаем библиотеку
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/cheathubtech/solara-cheat-menu/main/rayfield.lua'))()
+
+-- Загружаем наш функционал
+loadstring(game:HttpGet('https://raw.githubusercontent.com/cheathubtech/solara-cheat-menu/main/solara.lua'))()
